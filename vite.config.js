40d594df83e7b72037e8d8c49ddf5ad0018d76ae
tobysmvo <henocsmvo@gmail.com>
2026-05-26 11:35:23 +0200

    manual pwa manifest

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   plugins: [
     react(),
-
-    VitePWA({
-      registerType: "autoUpdate",
-
-      devOptions: {
-        enabled: true
-      },
-
-      includeAssets: [
-        "favicon.ico",
-        "apple-touch-icon.png",
-        "pwa-192x192.png",
-        "pwa-512x512.png"
-      ],
 
       manifest: {
         name: "HomèVo – Laboratoire Éco-Design",
